2a851ce7d868c76a09fdd4d349750e70a4726df3
Orlin <santiago.cruz43@gmail.com>
2019-08-07 14:44:37 -0600

    html

---
+++ conexion/conexion.php
@@ -0,0 +1,5 @@
+<?php
+
+
+$conexion=mysqli_connect('localhost','root','','programaciondenegocios') or
+                    die("Problemas con la conexion");
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>Ventana De Inicio</title>
+</head>
+<body>
+<h2>Bienvenidos al sistema de Alumnos</h2>
+<div class="alert alert-dark" role="alert">
+    A simple dark alert—check it out!
+</div>
+<a href="estudiantes">Alumnos</a>
+<a href="#">Cursos</a>
+</body>
+</html>
